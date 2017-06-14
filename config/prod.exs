56779ca5eab835b107abf00848df038467ed2c92
DMITRY VYSOTSKY <bigbenperl@gmail.com>
2017-06-15 00:13:34 +0300

    updates for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :meteobot, Meteobot.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "HTTPS", host: "multitempbot.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
