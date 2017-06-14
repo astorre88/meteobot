b5c4394a14879648b6ba69b236be7fcbd8c45dba
DMITRY VYSOTSKY <bigbenperl@gmail.com>
2017-06-15 00:46:08 +0300

    moved secret prod file

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,11 @@ config :meteobot, Meteobot.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "HTTPS", host: "multitempbot.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
+
+config :nadia,
+ token: "385233462:AAGR1k7-o4Jnz4tmKVjpttnPX3jjpC2xHs4"
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -59,4 +63,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
